5c07f5b955ff1788fa083f960809fb864ee6ad71
SumanBR321 <71173805+SumanBR321@users.noreply.github.com>
2024-01-06 12:28:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,58 +13,15 @@
     // })
 })();
 
-// const sections = document.querySelectorAll('.section');
-// const sectBtns = document.querySelectorAll('.controls');
-// const sectBtn = document.querySelectorAll('.control');  
-// const allSections = document.querySelector('.main-content');
+function downloadResume() {
+    const resumePath = 'https://drive.google.com/file/d/1eB6ZI6_4cCfuPThnE02By5n8_7CTL-LD/view?usp=sharing';
+    const downloadLink = document.createElement('a');
+    downloadLink.href = resumePath;
+    downloadLink.download = 'Sanidhya_Tiwari_Resume.pdf';
+  
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+  
+    document.body.removeChild(downloadLink);
+  }
 
-// function PageTransitions(){
-//     //button click active class
-//     for(  let i=0; i < sectBtn.length; i++){
-//         sectBtn[i].addEventListener('click', function(){
-//             let currentBtn = document.querySelectorAll('.active-btn');
-//             currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-//             this.className += 'active-btn';
-//         })
-//     }
-
-//     allSections.addEventListener('click',(e)=>{
-//         //console.log(e.target);
-//         const id = e.target.dataset.id;
-//         if(id){
-//             //remove selected from the other btns
-//             sectBtns.forEach((btn)=>{
-//                 btn.classList.remove('active');
-//             })
-//             e.target.classList.add('active');
-
-//             //hide other sections
-//             sections.forEach((section)=>{
-//                 section.classList.remove('active');
-//             })
-//             const element = document.getElementById(id);
-//             element.classList.add('active');
-//         }
-//     })
-// }
-
-// PageTransitions();
-
-// function submitForm() {
-//     // Collect form data
-//     var name = document.getElementById("name").value;
-//     var email = document.getElementById("email").value;
-//     var subject = document.getElementById("subject").value;
-//     var message = document.getElementById("message").value;
-
-//     //the syntax of the below code is form.addEventListener('type',f)
-//     const form = document.getElementById('contact-form');
-//     form.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//         //Display a message
-//         alert("Thank you, " + name + "! We will contact you shortly.\n\nDetails:\nEmail: " + email + "\nSubject: " + subject + "\nMessage: " + message);
-//         validateForm();
-//     });
-
-// }
-   
